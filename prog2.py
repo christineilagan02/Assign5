2b4848a8b66bb7a7146ede041450202ba91a6219
unknown <christineilagan02@gmail.com>
2021-11-14 08:37:09 +0800

    Find the lowest number is finalizing the input

--- prog2.py
+++ prog2.py
@@ -1,3 +1,16 @@
 # Create a program that ask 3 numbers. 
 # Find the lowest number using only if-else statement.
 # Display the lowest number
+
+tine = int(input("Enter the money of tine: "))
+jelenie = int(input("Enter the money of jelenie: "))
+jiyanie = int(input("Enter the money of jiyanie: "))
+
+if tine < jelenie and tine < jiyanie:
+    print("The lowest money is tine!")
+elif jelenie < tine and jelenie < jiyanie:
+    print("The lowest money is jelenie!")
+else:
+    print("The lowest money is jiyanie")
+
+print("May libre ka!")
